3ded3a29c27357c5c224876f16b986b78c5ea7bf
Anupam Anand <thewarlord858@gmail.com>
2020-06-26 18:44:22 +0530

    patientstore class added to database

--- dbschema.py
+++ dbschema.py
@@ -5,3 +5,17 @@ class UserStore(me.Document):
     user_id = me.StringField(required=True)
     password = me.StringField(required=True)
     time_stamp = me.DateTimeField()
+
+class PatientStore(me.Document):
+
+    ws_ssn = me.StringField(required=True)
+    ws_pat_id = me.StringField()
+    ws_pat_name = me.StringField(required=True)
+    ws_age = me.IntField(required=True)
+    ws_doj = me.DateTimeField(required=True)
+    ws_rtype = me.StringField(required=True)
+    ws_adrs = me.StringField(required=True)
+    ws_city = me.StringField(required=True)
+    ws_state = me.StringField(required=True)
+    ws_status = me.IntField()
+
